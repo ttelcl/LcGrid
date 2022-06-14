5f5450498bfda071e8a7083316840865d16c24e8
Luc Cluitmans <9429819+ttelcl@users.noreply.github.com>
2022-06-14 21:13:33 +0300

    Initial solution and bare Lcl.GridModel library

---
+++ Lcl.GridModel/LcGrid.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * (c) 2022  ttelcl / ttelcl
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lcl.GridModel
+{
+  /// <summary>
+  /// The central part of a grid data model
+  /// </summary>
+  public class LcGrid
+  {
+    /// <summary>
+    /// Create a new LcGrid
+    /// </summary>
+    public LcGrid()
+    {
+    }
+
+  }
+
+}
